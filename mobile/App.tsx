290b22384204fc7bd215eefa7c59ab0b263761be
Tiago A. Fontenele <tiagoarrfon@gmail.com>
2022-09-14 18:36:00 -0300

    add initial sources mobile in project

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello World!</Text>
+      <Button title='Press' style={styles.button}></Button>
+      <StatusBar style="auto" backgroundColor='#3433' />
     </View>
   );
+}
+
+interface ButtonProps{
+  title: string,
+  style?: object
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ff1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: 'blue',
+    fontSize: 33
+  },
+  button: {
+    borderColor: 'black',
+    backgroundColor: 'red'
+    
+  }
+
 });
