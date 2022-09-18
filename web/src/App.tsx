c2e0b46f5b0b861133b1a43cd9e95bd649603dc9
Tiago A. Fontenele <tiagoarrfon@gmail.com>
2022-09-18 14:00:09 -0300

    add button

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import "./styles/main.css"
 import logoImg from "./assets/logo.svg"
+import { MagnifyingGlassPlus } from "phosphor-react";
 
 function App() {
 
@@ -51,7 +52,16 @@ function App() {
           </div>
         </a>
       </div>
-      <div className="bg-[#2A2634] px-8 py-6 self-stretch mt-4">
+      <div className="pt-1 bg-nlw-gradiente rounded-lg self-stretch">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+          <strong className="tex-2xl text-white font-black block">Não encontrou o jogo?</strong>
+          <span className="text-zinc-400 block">Publique um anúncio para encontrar novos players!</span>
+          </div>
+          <button className="py-3 px-4 text-white bg-violet-500 rounded hover:bg-violet-600 flex items-center gap-3">
+            <MagnifyingGlassPlus size={24}/>
+            Publicar anúncio</button>
+        </div>
       </div>
     </div>
   )
