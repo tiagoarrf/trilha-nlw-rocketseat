18bb7b7c1fe8a367ba452174e3ba7b234346d44e
Tiago A. Fontenele <tiagoarrfon@gmail.com>
2022-09-18 13:10:40 -0300

    implmentation initia web screen 1 games

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,58 @@
-
-interface ButtonProps{
-  title ?: string
-}
-
-function Button(props: ButtonProps) {
-  return (
-    <button>
-      {props.title}
-    </button>
-  )
-}
+import "./styles/main.css"
+import logoImg from "./assets/logo.svg"
 
 function App() {
 
-  return <div>
-    <Button title="Send 1"></Button>
-    <Button title="Send 2"></Button>
-    <Button title="Send 3"></Button>
-  </div>
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-8">
+      <img className="h-40" src={logoImg} alt="" />
+      <h1 className="text-5xl text-white font-black">Seu <span className="bg-nlw-gradiente text-transparent bg-clip-text">duo</span> está aqui!</h1>
+      <div className="grid grid-cols-6 gap-6 mt-8">
+        <a href="" className="relative">
+          <img src="./src/assets/game1.png" alt="" />
+          <div className="w-full pt-8 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Leag of Legnds</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="./src/assets/game2.png" alt="" />
+          <div className="w-full pt-8 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Dota 2</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="./src/assets/game3.png" alt="" />
+          <div className="w-full pt-8 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Counter Strike</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="./src/assets/game4.png" alt="" />
+          <div className="w-full pt-8 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex Legnds</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="./src/assets/game5.png" alt="" />
+          <div className="w-full pt-8 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Fortnite</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="./src/assets/game6.png" alt="" />
+          <div className="w-full pt-8 pb-4 px-4 bg-game-gradiente absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">World Warcraft</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/img-fundo.png')",
+        "nlw-gradiente": 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 84.57%)',
+        "game-gradiente": 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+        
+      }
+    },
+  },
+  plugins: [],
+}
+
+
+
