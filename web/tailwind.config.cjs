e8da0e586e7fec00f14c81aefa3ffc0122972731
Tiago A. Fontenele <tiagoarrfon@gmail.com>
2022-09-18 14:08:06 -0300

    add font inter

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -5,12 +5,14 @@ module.exports = {
     "./index.html"
   ],
   theme: {
+    fontFamily:{
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         galaxy: "url('/img-fundo.png')",
         "nlw-gradiente": 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 84.57%)',
         "game-gradiente": 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
-        
       }
     },
   },
